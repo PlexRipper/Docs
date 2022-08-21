764c92056f9fc722b032767a032c192b2872c504
JasonLandbridge <jasonlandbridge@protonmail.com>
2022-08-21 15:46:20 +0200

    Changed default dev port to 4000 Changed to document driven mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
+// noinspection JSUnusedGlobalSymbols
+
 import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+  content: {
+    documentDriven: true,
+  },
 })
