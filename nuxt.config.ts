91b4f38ee51b12a18a7e0d420201a4d8bc641c90
JasonLandbridge <jasonlandbridge@protonmail.com>
2022-09-10 20:16:46 +0200

    Fixed Nuxt component import to not also include the directory names as prefixes Added Nuxt/Image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 import { defineNuxtConfig } from 'nuxt';
 import vuetify from 'vite-plugin-vuetify';
+import { fileURLToPath } from 'url';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	modules: [
 		'@nuxt/content',
+		'@nuxt/image-edge',
 		async (options, nuxt) => {
 			// @ts-ignore
 			// Source: https://www.the-koi.com/projects/how-to-set-up-a-project-with-nuxt3-and-vuetify3-with-a-quick-overview/
@@ -25,5 +27,27 @@ export default defineNuxtConfig({
 		ssr: {
 			noExternal: ['vuetify'], // add the vuetify vite plugin
 		},
+	},
+	/*
+	 ** Auto-import components
+	 *  Doc: https://github.com/nuxt/components
+	 */
+	components: {
+		loader: true,
+		dirs: [
+			// Components directory
+			{
+				path: './components',
+				pathPrefix: false,
+				extensions: ['vue'],
+			},
+		],
+	},
+	// https://v1.image.nuxtjs.org/configuration/
+	image: {
+		dir: 'assets/img',
+	},
+	alias: {
+		img: fileURLToPath(new URL('./assets/img', import.meta.url)),
 	},
 });
