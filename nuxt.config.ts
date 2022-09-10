9848a681332ca1fb291d6b174b33357bf0ef52fc
JasonLandbridge <jasonlandbridge@protonmail.com>
2022-09-10 14:09:17 +0200

    Installed Vuetify v3 Imported JetBrains gitignore Installed Prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,20 @@
 // noinspection JSUnusedGlobalSymbols
 
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
-  content: {
-    documentDriven: true,
-  },
-})
+	modules: ['@nuxt/content'],
+	content: {
+		documentDriven: true,
+	},
+	css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
+	build: {
+		transpile: ['vuetify'],
+	},
+	vite: {
+		define: {
+			'process.env.DEBUG': false,
+		},
+	},
+});
