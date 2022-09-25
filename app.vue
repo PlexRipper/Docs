d2f805b4754d2a5d0f057e709957e9224106e747
JasonLandbridge <jasonlandbridge@protonmail.com>
2022-09-25 22:34:23 +0200

    Added favicon

--- app.vue
+++ app.vue
@@ -2,9 +2,18 @@
 import { useNavigationStore } from "store/navigationStore";
 import Background from '~/components/General/Background.vue'
 import AppBar from "~/components/AppBar/AppBar.vue";
+import { useHead } from "#imports";
 
 const store = useNavigationStore();
 await store.setup()
+
+useHead({
+  title: 'PlexRipper Docs',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [{ name: 'description', content: 'Documentation website for the PlexRipper project' }],
+  link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+})
 
 </script>
 
