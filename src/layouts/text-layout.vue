ec008f993aad3899b84b552cae89d1a7ea00bcc3
JasonLandbridge <jasonlandbridge@protonmail.com>
2022-10-22 21:56:01 +0200

    Improved the announcement detail page with banner, date and authors displayed

---
+++ src/layouts/text-layout.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <v-main class="mt-10">
+    <v-container>
+      <v-row justify="center" no-gutters>
+        <v-col cols="12" lg="11" xl="6">
+          <slot/>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
