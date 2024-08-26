802897772958ee5c9a8a25c5a80d34af2b2c65a3
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-08-26 09:41:09 +0200

    build(Web-UI): setup primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,69 +1,90 @@
 import vuetify from 'vite-plugin-vuetify';
 import { defineNuxtConfig } from 'nuxt/config';
 import { fileURLToPath } from 'url';
+import Aura from '@primevue/themes/aura';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    // Doc: https://v3.nuxtjs.org/api/configuration/nuxt.config#srcdir
-    srcDir: 'src/',
-    ssr: true,
-    content: {
-        documentDriven: true,
-        markdown: {
-            anchorLinks: false,
-        },
-        highlight: {
-            theme: 'github-dark',
-            preload: ['ts', 'js', 'css', 'json', 'bash'],
-        }
-    },
-    css: ['vuetify/styles', '@/assets/scss/style.scss'],
-    build: {
-        // extractCSS: true,
-        transpile: ['vuetify'],
-    },
-    vite: {
-        ssr: {
-            noExternal: ['vuetify'],
-        },
-        define: {
-            'process.env.DEBUG': false,
-        },
-    },
-    modules: [
-        '@nuxt/content',
-        '@pinia/nuxt',
-        '@nuxtjs/plausible',
-        async (options, nuxt) => {
-            // @ts-ignore
-            // Source: https://www.the-koi.com/projects/how-to-set-up-a-project-with-nuxt3-and-vuetify3-with-a-quick-overview/
-            nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(vuetify({ styles: 'sass' })));
-        },
-    ],
-    plausible: {
-        autoOutboundTracking: true,
-        // domain: 'plexripper.rocks'
-    },
-    /*
-     ** Auto-import components
-     *  Doc: https://github.com/nuxt/components
-     */
-    components: {
-        loader: true,
-        dirs: [
-            // Components directory
-            {
-                path: './components',
-                pathPrefix: false,
-                global: true,
-                extensions: ['vue'],
-            },
-        ],
-    },
-    alias: {
-        img: fileURLToPath(new URL('./src/assets/img', import.meta.url)),
-        const: fileURLToPath(new URL('./src/common/constants', import.meta.url)),
-        store: fileURLToPath(new URL('./src/store', import.meta.url)),
-        components: fileURLToPath(new URL('./src/components', import.meta.url)),
-    },
+  // Doc: https://v3.nuxtjs.org/api/configuration/nuxt.config#srcdir
+  srcDir: 'src/',
+
+  ssr: true,
+
+  content: {
+      documentDriven: true,
+      markdown: {
+          anchorLinks: false,
+      },
+      highlight: {
+          theme: 'github-dark',
+          preload: ['ts', 'js', 'css', 'json', 'bash'],
+      }
+  },
+
+  css: ['vuetify/styles', '@/assets/scss/style.scss'],
+
+  build: {
+      // extractCSS: true,
+      transpile: ['vuetify'],
+  },
+
+  vite: {
+      ssr: {
+          noExternal: ['vuetify'],
+      },
+      define: {
+          'process.env.DEBUG': false,
+      },
+  },
+
+  modules: [
+      '@nuxt/content',
+      '@pinia/nuxt',
+      '@nuxtjs/plausible',
+      '@primevue/nuxt-module',
+      async (options, nuxt) => {
+          // @ts-ignore
+          // Source: https://www.the-koi.com/projects/how-to-set-up-a-project-with-nuxt3-and-vuetify3-with-a-quick-overview/
+          nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(vuetify({ styles: 'sass' })));
+      },
+  ],
+
+  primevue: {
+      options: {
+          theme: {
+              preset: Aura
+          }
+      }
+  },
+
+  plausible: {
+      autoOutboundTracking: true,
+      // domain: 'plexripper.rocks'
+  },
+
+  /*
+   ** Auto-import components
+   *  Doc: https://github.com/nuxt/components
+   */
+  components: {
+      loader: true,
+      dirs: [
+          // Components directory
+          {
+              path: './components',
+              pathPrefix: false,
+              global: true,
+              extensions: ['vue'],
+          },
+      ],
+  },
+
+  alias: {
+      img: fileURLToPath(new URL('./src/assets/img', import.meta.url)),
+      const: fileURLToPath(new URL('./src/common/constants', import.meta.url)),
+      store: fileURLToPath(new URL('./src/store', import.meta.url)),
+      components: fileURLToPath(new URL('./src/components', import.meta.url)),
+  },
+
+  compatibilityDate: '2024-08-26',
 });
